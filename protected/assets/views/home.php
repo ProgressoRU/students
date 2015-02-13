3c87eea9486b2b9615df6a66c52acdaaaa5ec1c8
codels <codels25@gmail.com>
2015-02-13 18:51:39 +0300

    update

--- protected/assets/views/home.php
+++ protected/assets/views/home.php
@@ -14,46 +14,7 @@
     <script src="./public/js/controllers.js"></script>
 </head>
 <body>
-<header>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="navbar-inner">
-            <a class="brand" href="#">Title</a>
-            <ul class="nav" role="navigation">
-                <li class="dropdown">
-                    <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                    <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="http://google.com">Action</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#anotherAction">Another action</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-                        <li role="presentation" class="divider"></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" id="drop2" role="button" class="dropdown-toggle" data-toggle="dropdown">Dropdown 2 <b class="caret"></b></a>
-                    <ul class="dropdown-menu" role="menu" aria-labelledby="drop2">
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-                        <li role="presentation" class="divider"></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <ul class="breadcrumb">
-            <li><a href="#">Home</a> <span class="divider">/</span></li>
-            <li><a href="#">Library</a> <span class="divider">/</span></li>
-            <li class="active">Data</li>
-        </ul>
-    </div>
-</header>
-<p>asd</p>
-
-<div ng-controller="MenuCtrl">
-    <ul>
-        <li ng-repeat="phone in phones">{{phone.name}}</li>
-    </ul>
-</div>
+123
+<div ng-view></div>
 </body>
 </html>
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('Students', []);
+var app = angular.module('Students', [])
+    .config(function($routeProvider) {
+    $routeProvider.
+        when('/', {controller:HomeCtrl, templateUrl:'public/views/home.html'}).
+        when('/login', {controller:LoginCtrl, templateUrl:'public/views/login.html'}).
+        otherwise({redirectTo:'/'});
+});
+
+function HomeCtrl($scope) {
+
+}
+
+function LoginCtrl($scope) {
+
+}
