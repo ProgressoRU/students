f551f6b631fae54c4f6e15bd985414b0b15f7b4c
Victor <progresso.ptz@gmail.com>
2015-02-15 22:49:27 +0300

    Updated DB structure, added separate .sql with data

--- protected/sql/students(withData).sql
+++ protected/sql/students(withData).sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50523
 File Encoding         : 65001
 
-Date: 2015-02-15 22:47:55
+Date: 2015-02-15 22:48:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -35,6 +35,10 @@ CREATE TABLE `tblarticles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of tblarticles
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for tblattachments
 -- ----------------------------
 DROP TABLE IF EXISTS `tblattachments`;
@@ -53,6 +57,10 @@ CREATE TABLE `tblattachments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of tblattachments
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for tblclasses
 -- ----------------------------
 DROP TABLE IF EXISTS `tblclasses`;
@@ -66,6 +74,20 @@ CREATE TABLE `tblclasses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of tblclasses
+-- ----------------------------
+INSERT INTO `tblclasses` VALUES ('1', 'Очень интересный предмет', '222', '1');
+INSERT INTO `tblclasses` VALUES ('2', 'Еще интересный предмет', '333', '2');
+INSERT INTO `tblclasses` VALUES ('3', 'Больше предметов', null, '1');
+INSERT INTO `tblclasses` VALUES ('4', 'Архитектура ЭВМ', null, '1');
+INSERT INTO `tblclasses` VALUES ('5', 'Базы Данных', null, '1');
+INSERT INTO `tblclasses` VALUES ('6', 'фывфывыфв', null, '2');
+INSERT INTO `tblclasses` VALUES ('7', 'ывыфввы', null, '2');
+INSERT INTO `tblclasses` VALUES ('8', 'Информационные технологии', null, '3');
+INSERT INTO `tblclasses` VALUES ('9', 'Диплом', null, '3');
+INSERT INTO `tblclasses` VALUES ('10', 'вфыывф', null, '3');
+
+-- ----------------------------
 -- Table structure for tblcourses
 -- ----------------------------
 DROP TABLE IF EXISTS `tblcourses`;
@@ -75,6 +97,13 @@ CREATE TABLE `tblcourses` (
   `intCourseNum` int(11) unsigned NOT NULL,
   PRIMARY KEY (`CourseID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of tblcourses
+-- ----------------------------
+INSERT INTO `tblcourses` VALUES ('1', 'ИС', '2');
+INSERT INTO `tblcourses` VALUES ('2', 'ИС', '3');
+INSERT INTO `tblcourses` VALUES ('3', 'ИС', '4');
 
 -- ----------------------------
 -- Table structure for tblgroups
@@ -89,6 +118,10 @@ CREATE TABLE `tblgroups` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of tblgroups
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for tblnews
 -- ----------------------------
 DROP TABLE IF EXISTS `tblnews`;
@@ -100,6 +133,25 @@ CREATE TABLE `tblnews` (
   `dateCreated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`newsID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tblnews
+-- ----------------------------
+INSERT INTO `tblnews` VALUES ('1', 'Обычная новость', 'Текст новости', '0', '2015-02-15 02:23:14');
+INSERT INTO `tblnews` VALUES ('2', 'Важная новость', '2', '1', '2015-02-15 01:51:02');
+INSERT INTO `tblnews` VALUES ('3', 'Крайне важная новость с крайне длиинным заголовком', '3', '2', '2015-02-15 01:51:23');
+INSERT INTO `tblnews` VALUES ('4', 'Еще новость', 'фвлфывлфыволыфво', '0', '2015-02-15 17:49:12');
+INSERT INTO `tblnews` VALUES ('5', 'фвыфыв', 'фываыфвфв', '0', '2015-02-15 17:49:11');
+INSERT INTO `tblnews` VALUES ('6', 'Дополнительная новость', 'ыфвфыв', '1', '2015-02-15 17:49:10');
+INSERT INTO `tblnews` VALUES ('7', 'Последняя нечетная новость', 'фывывфвыф', '2', null);
+INSERT INTO `tblnews` VALUES ('8', 'Новость для провери', 'щщзщзцлкуцщокшуцощшцуоашщоцшмоашыовалытвфадтыфва', '1', null);
+INSERT INTO `tblnews` VALUES ('9', 'фывошфыв', 'фывыфв', '0', null);
+INSERT INTO `tblnews` VALUES ('10', 'jsdaasdsda', 'asdsad', '1', null);
+INSERT INTO `tblnews` VALUES ('11', 'wwwwwwww', 'wwwwwwww', '0', null);
+INSERT INTO `tblnews` VALUES ('12', 'sdskadjkjsad', 'wwdadkladkjsadkjsakdljsalkdjsknjvnsdkjlfheuiyhuhsid', '3', null);
+INSERT INTO `tblnews` VALUES ('13', 'Test ', 'sfjskdj', '1', null);
+INSERT INTO `tblnews` VALUES ('14', 'Test 2 ', 'test', '2', null);
+INSERT INTO `tblnews` VALUES ('15', 'Last news everyone!', 'Pager is totally working now!', '2', null);
 
 -- ----------------------------
 -- Table structure for tblusers
@@ -119,3 +171,8 @@ CREATE TABLE `tblusers` (
   KEY `GroupID` (`GroupID`),
   CONSTRAINT `tblusers_ibfk_1` FOREIGN KEY (`GroupID`) REFERENCES `tblgroups` (`GroupID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tblusers
+-- ----------------------------
+INSERT INTO `tblusers` VALUES ('1', 'login', '35f504164d5a963d6a820e71614a4009', 'sad', null, null, null, '8e27f7e2164d641902d0261acf702b95', '127.0.0.1');
