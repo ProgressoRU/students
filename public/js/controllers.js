40337f892918c5f000105556ef6f0047ffab9770
Victor <progresso.ptz@gmail.com>
2015-02-15 17:46:23 +0300

    News pager

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -17,24 +17,36 @@ stCtrls.controller('HeaderCtrl',['$scope','$http',
 stCtrls.controller('NewsCtrl',['$scope','$http',
     function($scope,$http) {
         $http.get('api/news/all').success(function(data){
-            $scope.news=data.news;
-        })  
+            //$scope.news = data.news;
+            $scope.length = data.news.length;
+            $scope.totalPages = $scope.length / 3;
+            $scope.CurPage = 1;
+            $scope.lastNews = function(k)
+            {
+                $scope.visNews = new Array();
+                $scope.startPos = $scope.length - $scope.CurPage * k;
+                j = 0;
+                    for(i=$scope.startPos;j<k;j++,i++)
+                    $scope.visNews[j] = data.news[i];
+                return $scope.visNews.reverse();
+            }
+            $scope.prevPage = function()
+            {
+                if ($scope.CurPage < $scope.totalPages)
+                $scope.CurPage+=1;
+            }
+            $scope.nextPage = function()
+            {
+                if ($scope.CurPage > 1)
+                    $scope.CurPage-=1;
+            }
+            console.log($scope.lastNews(3));
+            console.log($scope.totalPages);
+            console.log($scope.length);
+        })
     }]);
 
-
 /*
-function HomeCtrl($scope, $http) {
-$http({method: 'GET', url: 'api/courses/all'}).success(function(data) {
-            $scope.courses = data.courses;
-            console.log($scope.courses);
-        })};
-    
-$http({method: 'GET', url: 'api/classes/all'}).success(function(data) {
-            $scope.classes = data.classes;
-            console.log($scope.classes);
-        });
-    }
-
 function LoginCtrl($scope) {
 
 } */
