7b99eb8f9bc3523f493efeb8c347dbe7577949c1
ProgressoRU <progresso.ptz@gmail.com>
2015-02-26 15:36:20 +0300

    Fix errors in UserAPI

--- protected/classes/App/Controller/ApiUser.php
+++ protected/classes/App/Controller/ApiUser.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Libraries\Request,
+    //App\Libraries\Auth,
     Exception;
 
 class ApiUser extends \App\Page {
@@ -27,18 +28,22 @@ class ApiUser extends \App\Page {
         catch (Exception $e)
         {
             $response['message'] = 'SQL Error\nIt\'s might help:\n'.$e->getMessage();
+            $this->view->response = $response;
         }
         //echo ;
         if ($response['user'] != null)
         {
+            $response['status'] = 1;
+            $response['message'] = 'Success';
             $hash = md5(uniqid(rand(),true));
             try
             {
                 $this->pixie->db->query('update')->table('tblusers')
-                    ->data(array('sessionHash' => $hash ))
-                    ->data(array('lastIP'=>$_SERVER['REMOTE_ADDR']))
-                    ->where('id', $response['user'][0]->uID)
-                    ->where('username',$login)
+                    ->data(array(
+                        'sessionHash' => $hash,
+                        'lastIp'=>$_SERVER['REMOTE_ADDR'],
+                        'useragent'=>$_SERVER['HTTP_USER_AGENT']))
+                    ->where('uid', $response['user'][0]->uID)
                     ->execute();
             }
             catch (Exception $e)
@@ -48,8 +53,6 @@ class ApiUser extends \App\Page {
                 //Устанавливем куки (на час)
                 setcookie("id", $response['user'][0]->uID, time()+3600);
                 setcookie("hash", $hash, time()+3600);
-                $response['status'] = 1;
-                $response['message'] = 'Success';
         }
         else $response['message'] = 'User not found';
 
