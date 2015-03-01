f4a8781760ac61e21671f733bfaeb9d32d779547
ProgressoRU <progresso.ptz@gmail.com>
2015-03-01 05:42:28 +0300

    Login modal animation

--- public/js/directives.js
+++ public/js/directives.js
@@ -9,13 +9,20 @@ stDirectives.directive('stHeader', function () {
 stDirectives.directive('loginDialog', function (AUTH_EVENTS) {
     return {
         restrict: 'A',
-        template: '<div data-ng-class="{show: visible, hide:!visible}" class="modal" data-ng-if="visible" data-ng-include="\'public/views/login.html\'">',
+        template: '<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginLabel" aria-hidden="true" data-ng-if="visible" data-ng-include="\'public/views/login.html\'">',
+        controller: 'LoginModalCtrl',
         link: function (scope) {
             var showDialog = function () {
                 scope.visible = true;
+                $('#loginModal').modal({
+                    backdrop: 'static',
+                    keyboard: false,
+                    show: true
+                });
             };
             var hideDialog = function () {
                 scope.visible = false;
+                $('loginModal').hide();
             };
 
             scope.visible = false;
